7d4cc047ebf4a11308e0806235097e7b606784eb
Bipson Shrestha <bipsonshrestha@Macbook-Pro-2.local>
2026-05-28 16:57:04 +0545

    added initial features

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use((err,req,res,next)=>{
     })
 })
 
-app.listen(process.env.PORT,process.env.HOSTNAME,()=>{
+const PORT=process.env.PORT || 3000;
+app.listen(PORT,()=>{
     console.log(`The server is running`);
 })
